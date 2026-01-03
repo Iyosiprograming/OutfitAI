ba4006b688478535b2c1d0e1aa7403c5baa9bb1e
Iyosiyas <iyosieyosiyas@gmail.com>
2026-01-03 18:14:58 +0300

    init:created frontend with react and tailwind

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <h1 className="text-6xl font-sans text-blue-600">
+        Tailwind Works!
+      </h1>
+    </div>
+  );
+}
